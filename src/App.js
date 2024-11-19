7f045615a56c78c1454009b945caa159dbd8222c
Fadhlina Wjedi <128072211+suwwui@users.noreply.github.com>
2024-11-20 00:29:11 +0800

    ui-update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { generateClient } from "aws-amplify/api";
 import { createChat } from './graphql/mutations';
 import { listChats, getChat } from "./graphql/queries";
 import * as subscriptions from "./graphql/subscriptions";
+import { onCreateChat } from "./graphql/subscriptions";
 Amplify.configure(awsconfig);
 const client = generateClient()
 
@@ -48,22 +49,7 @@ function App({ user, signOut }) {
   const [chats, setChats] = useState([]);
   const [message, setMessage] = useState("");
   const userEmail = user?.attributes?.email || "";
-  const [users, setUsers] = useState([]); 
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const result = await client.graphql({
-  //         query:listUsers,}); // Assuming listUsers fetches all users
-  //       setUsers(listUsers.data.listUsers.items);
-  //     } catch (err) {
-  //       console.error("Error fetching users:", err);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  //  }, []);
-
+  
   // Fetch chat messages when the component mounts
   useEffect(() => {
     const fetchChats = async () => {
@@ -71,16 +57,14 @@ function App({ user, signOut }) {
         const allChats = await client.graphql({
           query: listChats,
         });
-        console.log(allChats.data.listChats.items);
         setChats(allChats.data.listChats.items);
       } catch (error) {
         console.error("Error fetching chats:", error);
       }
     };
-
-    fetchChats();
-  }, []);
-
+  fetchChats();
+    }, []);
+  
   const handleSendMessage = async () => {
     if (message.trim() === "") return; // Don't send empty messages
 
@@ -94,7 +78,7 @@ function App({ user, signOut }) {
           },
         },
       });
-      setMessage(""); // Clear the input field
+      setMessage(" ");
       // Re-fetch chats after sending a message
       const allChats = await client.graphql({
         query: listChats,
@@ -105,6 +89,7 @@ function App({ user, signOut }) {
     }
   };
 
+const uniqueEmails = [...new Set(chats.map(chat => chat.email))];
 
   return (
     <div>
@@ -119,10 +104,34 @@ function App({ user, signOut }) {
       </div>
 
       
-
-      <div className="flex justify-center items-center h-screen w-full">
+     <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+         <li>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+               </svg>
+               <span class="ms-3">Active User</span>
+            </a>
+         </li>
+         {uniqueEmails.map((email, index) => (
+              <li key={index}>
+                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <span className="flex-1 ms-3 whitespace-nowrap">{email.split("@")[0]}</span>
+                </a>
+              </li>
+            ))}
+      </ul>
+   </div>
+</aside>
+     <div class="p-4 sm:ml-64">
       
-      <div className={`w-3/4 flex flex-col`}>
+      <div className={`flex flex-col p-4 border-2 border-gray-200`}> 
+       <div  class="flex items-center justify-center rounded bg-gray-50 "></div>
+          
+            <div/>
            {/* Displaying chat messages */}
           {chats
           .sort((a, b) => a.createdAt.localeCompare(b.createdAt))
@@ -134,10 +143,10 @@ function App({ user, signOut }) {
                 >
     
               <div>
-                  <div className="flex justify-between gap-x-4">
+                  <div className="flex justify-between gap-x-4"> 
                     <div className="py-0.5 text-xs leading-5 text-gray-500">
                       <span className="font text-gray-900">
-                        {chat.email.split("@")[0]}
+                        {chat.email === userEmail ? "Me" : chat.email.split("@")[0]}
                       </span>{" "}
                     </div>
                     <time
@@ -153,25 +162,19 @@ function App({ user, signOut }) {
           ))}
           
           <div>
-            <div className="relative mt-2 flex items-center">
+            <div className="relative mt-2 mb-3 flex items-center">
               <input
                 type="text"
                 name="message"
                 id="message"
-                onKeyUp={async (e) => {
-                  if (e.key === "Enter") {
-                    await client.graphql({
-                      query: createChat,
-                      variables: {
-                        input: {
-                          text: e.target.value,
-                          email: userEmail,
-                        },
-                      },
-                    });
-                    e.target.value = "";
-                  }
-                }}
+               onChange={(e) => setMessage(e.target.value)}
+              onKeyUp={async (e) => {
+                if (e.key === "Enter") {
+                  await handleSendMessage();
+                }
+              }}
+              placeholder="  type a message..."
+              
                 className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
               <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
